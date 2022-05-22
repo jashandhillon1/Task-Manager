11ed54198a41260be19a99c29f071b52af9f2b57
jashan dhillon <jashandhillon211@gmail.com>
2022-05-22 17:51:13 +0530

    create document in database

---
+++ task-manager/mongodb.js
@@ -0,0 +1,20 @@
+const mongodb=require('mongodb')
+const MongoClient=mongodb.MongoClient
+
+const connectURL='mongodb://127.0.0.1:27017'
+const databaseName='task-manager'
+
+
+MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client)=>{
+    if(error){
+        return console.log('Unable to connect to database')
+
+    }
+    const db=client.db(databaseName)
+    db.collection('users').insertOne({
+        name:'Jashanpreet Singh',
+        age:23
+    })
+
+})
+
