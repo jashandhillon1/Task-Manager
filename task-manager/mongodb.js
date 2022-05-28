95a24cc965f8d2254ee50af3596d99b5378752e1
jashan dhillon <jashandhillon211@gmail.com>
2022-05-28 18:10:57 +0530

    Insert as many in mongo database

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,8 +1,17 @@
 const mongodb=require('mongodb')
-const MongoClient=mongodb.MongoClient
+// const MongoClient=mongodb.MongoClient
+// const ObjectID=mongo.ObjectID
+
+
+const {MongoClient,ObjectID}=require('mongodb')
 
 const connectURL='mongodb://127.0.0.1:27017'
 const databaseName='task-manager'
+
+
+const id=new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
 
 
 MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client)=>{
@@ -10,10 +19,61 @@ MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client)=>{
         return console.log('Unable to connect to database')
 
     }
+    console.log('Connected')
     const db=client.db(databaseName)
     db.collection('users').insertOne({
-        name:'Jashanpreet Singh',
+        
+        name:'vikramś',
         age:23
+    },(error,result)=>{
+        if(error){
+            return console.log('Unable to insert User')
+
+        }
+
+        console.log(result.acknowledged)
+
+    })
+
+
+    db.collection('users').insertMany([
+        {
+            name:'jen',
+            age:28
+        },
+        {
+            name:'love',
+            age:26
+        }
+    ],(error,result)=>{
+        if(error){
+            return console.log("unable to put docs")
+        }
+
+        console.log(result.acknowledged)
+
+
+    })
+
+
+    db.collection('tasks').insertMany([
+        {description:'Clean the house',
+        completed:true
+
+        },
+        {
+            description:'Renew the inspection',
+            completed:false
+        },
+        {
+            description:'Learn Cp',
+            completed:false
+        }
+    ],(error,result)=>{
+        if(error){
+            return console.log('Unable to put')
+        }
+        console.log(result.acknowledged)
     })
 
 })
